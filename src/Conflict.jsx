57b22699dfda381401a77d0754d5ef470e07b530
Hari Krishnan <hari.kizhakkillom@cgi.com>
2023-09-04 14:30:26 -0400

    commit 4

--- src/Conflict.jsx
+++ src/Conflict.jsx
@@ -1,20 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { Message } from "semantic-ui-react";
 import { Button, Form } from "semantic-ui-react";
+import axios from "axios";
 
 const Conflict = () => {
+  const [emailSuccess, setEmailSuccess] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+  const [progress, setProgress] = useState(false);
+  const sendEmail = () => {
+    setProgress(true);
+    axios
+      .post(
+        "https://humber-poc-cgi.workflows.okta.com/api/flo/d856674875d68127e1c45e8daa479556/invoke",
+        {
+          alternateemail: userEmail,
+        }
+      )
+      .then((res) => {
+        setProgress(false);
+        setEmailSuccess(true);
+      })
+      .catch((err) => {
+        setEmailSuccess(false);
+        console.error("ERR", err);
+      });
+  };
+
   return (
     <div className="general-wrapper">
-      <Message color="yellow">
-        <Message.Header>
-          Do you have any other login credential (eg: Email )
-        </Message.Header>
-        <p>If so, please enter the email id in the below textbox.</p>
-      </Message>
-      <Form success>
-        <Form.Input label="Email ID" placeholder="Your email " />
-        <Button>Submit</Button>
-      </Form>
+      {!emailSuccess && (
+        <>
+          <Message color="yellow">
+            <Message.Header>
+              It seems you already have another registered email / credential
+              with us already.
+            </Message.Header>
+            <p>
+              Please enter the email id in the below textbox. We will send a
+              verification code to the email to consolidate the accounts
+            </p>
+          </Message>
+          <Form success>
+            <Form.Input
+              label="Email ID"
+              placeholder="Your email"
+              onChange={(e) => setUserEmail(e.target.value)}
+            />
+            <Button onClick={sendEmail}>Submit</Button>
+          </Form>
+        </>
+      )}
+
+      {progress && (
+        <>
+          <h1> Sending the email...</h1>
+          <div className="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </>
+      )}
+      {emailSuccess && (
+        <>
+          <Message color="green">
+            <Message.Header>Success!</Message.Header>
+            <p>
+              You would have received an email in your Inbox/Spam. The accounts
+              will be consolidated from our side. You don't need to do anything
+              from your side, we will keep you updated once the consolidation is
+              done.
+            </p>
+          </Message>
+        </>
+      )}
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,26 @@
  * "IMPORTANT: THIS FILE IS GENERATED, CHANGES SHOULD BE MADE WITHIN '@okta/generator'"
  **/
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
-import getEnvModule from './env'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import getEnvModule from "./env";
 
 getEnvModule().setEnvironmentVarsFromTestEnv(__dirname);
 
 process.env.CLIENT_ID = process.env.SPA_CLIENT_ID || process.env.CLIENT_ID;
-process.env.OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
+process.env.OKTA_TESTING_DISABLEHTTPSCHECK =
+  process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
 process.env.USE_INTERACTION_CODE = process.env.USE_INTERACTION_CODE || false;
 
 const env = {};
 
 // List of environment variables made available to the app
 [
-  'ISSUER',
-  'CLIENT_ID',
-  'OKTA_TESTING_DISABLEHTTPSCHECK',
-  'USE_INTERACTION_CODE',
+  "ISSUER",
+  "CLIENT_ID",
+  "OKTA_TESTING_DISABLEHTTPSCHECK",
+  "USE_INTERACTION_CODE",
 ].forEach((key) => {
   if (!process.env[key]) {
     throw new Error(`Environment variable ${key} must be set. See README.md`);
@@ -29,33 +30,39 @@ const env = {};
 });
 
 // https://vitejs.dev/config/
-export default defineConfig(( { command } ) => {
-  const includeSampleBaseName = command === 'build' && !process.env.STANDALONE_SAMPLE_BUILD;
+export default defineConfig(({ command }) => {
+  const includeSampleBaseName =
+    command === "build" && !process.env.STANDALONE_SAMPLE_BUILD;
   return {
-    base: includeSampleBaseName ? '/custom-login/' : '/',
+    base: includeSampleBaseName ? "/custom-login/" : "/",
     plugins: [react()],
     define: {
-      'process.env': env
+      "process.env": env,
     },
     resolve: {
       alias: {
-        'react-router-dom': path.resolve(__dirname, 'node_modules/react-router-dom')
-      }
+        "react-router-dom": path.resolve(
+          __dirname,
+          "node_modules/react-router-dom"
+        ),
+      },
     },
     server: {
-      port: process.env.PORT || 8080
+      port: process.env.PORT || 8080,
     },
     preview: {
-      port: process.env.PORT || 8080
+      port: process.env.PORT || 8080,
     },
     build: {
       rollupOptions: {
         // always throw with build warnings
-        onwarn (warning, warn) {
-          warn('\nBuild warning happened, customize "onwarn" callback in vite.config.js to handle this error.');
+        onwarn(warning, warn) {
+          warn(
+            '\nBuild warning happened, customize "onwarn" callback in vite.config.js to handle this error.'
+          );
           throw new Error(warning);
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  };
 });
